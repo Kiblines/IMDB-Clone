b9c18c37b9b15880a1655359416cea5eada6bb7f
Anthony Lines <linesanthony018@gmail.com>
2023-08-19 14:53:10 +0200

    Structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import HomePage from "./pages/HomePage";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return <HomePage></HomePage>;
 }
 
 export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Logo from "../assets/images/lines_carre.png";
+
+export default function HomePage() {
+  return (
+    <div>
+      <img src={Logo} alt="IMDB Logo" />
+    </div>
+  );
+}
