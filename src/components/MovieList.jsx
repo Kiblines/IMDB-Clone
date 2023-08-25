22d5d7ed459b89ff9ccd596dd1edb31de806200d
Anthony Lines <linesanthony018@gmail.com>
2023-08-25 18:19:36 +0200

    list & prerooting

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,5 +1,29 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components"; // Importe le module styled-components
 import { getPopularMovies } from "../api/Imdb";
+
+// Crée des composants Styled Components
+const MovieItem = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+const Poster = styled.div`
+  margin-right: 20px;
+`;
+
+const MovieDetails = styled.div``;
+
+const MovieTitle = styled.h2`
+  margin-bottom: 10px;
+`;
+
+const ReleaseDate = styled.p`
+  margin-bottom: 5px;
+  font-weight: bold;
+`;
+
+const Overview = styled.p``;
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
@@ -11,6 +35,7 @@ const MovieList = () => {
         setMovies(moviesData);
       } catch (error) {
         console.error("Error fetching data:", error);
+        setMovies([]);
       }
     };
 
@@ -20,24 +45,23 @@ const MovieList = () => {
   return (
     <div>
       <h1>Popular Movies</h1>
-      <ul>
-        {movies.map((movie) => (
-          <li key={movie.id}>
-            <div>
+      {movies.map((movie) => (
+        <MovieItem key={movie.id}>
+          {movie.poster_path && (
+            <Poster>
               <img
                 src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
                 alt={movie.title}
               />
-            </div>
-            <div>
-              <h2>{movie.title}</h2>
-              <p>Release Date: {movie.release_date}</p>
-              <p>{movie.overview}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-      <p>Ceci est la fin de movie list</p>
+            </Poster>
+          )}
+          <MovieDetails>
+            <MovieTitle>{movie.title}</MovieTitle>
+            <ReleaseDate>Release Date: {movie.release_date}</ReleaseDate>
+            <Overview>{movie.overview}</Overview>
+          </MovieDetails>
+        </MovieItem>
+      ))}
     </div>
   );
 };
