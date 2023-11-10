ea018e304d675170256fce812942c86c6749054f
Anthony Lines <linesanthony018@gmail.com>
2023-11-10 13:18:56 +0100

    Config react query

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,9 @@
 import Navbar from "../components/Navbar";
 import styled from "styled-components";
 import MovieList from "../components/MovieList";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const HomeContainer = styled.div`
   display: flex;
@@ -9,16 +12,18 @@ const HomeContainer = styled.div`
 const NavbarWrapper = styled.div`
   position: fixed;
   top: 0; /* Fixer la barre de navigation en haut de la page */
-  width: 100%; /* Prend la largeur complète */
+  width: 100%;
 `;
 
 export default function HomePage() {
   return (
-    <HomeContainer>
-      <NavbarWrapper>
-        <Navbar></Navbar>
-      </NavbarWrapper>
-      <MovieList />
-    </HomeContainer>
+    <QueryClientProvider client={queryClient}>
+      <HomeContainer>
+        <NavbarWrapper>
+          <Navbar></Navbar>
+        </NavbarWrapper>
+        <MovieList />
+      </HomeContainer>
+    </QueryClientProvider>
   );
 }
